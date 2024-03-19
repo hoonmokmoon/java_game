147ab4621a5a5cec52dd665730c3aa3906aa964b
hoonmok.moon <hoonmok.moon@gmail.com>
2024-03-19 23:54:56 +0900

    code clean

--- src/ch1/GameJava1_08_JFrame.java
+++ src/ch1/GameJava1_08_JFrame.java
@@ -5,27 +5,22 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel; 
 
-public class GameJava1_08_JFrame extends JFrame {
+public class GameJava1_08_JFrame extends JFrame {  
+	private static final long serialVersionUID = -2806750967539824371L;
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;  
-	
 	public static void main(String[] args) {
 		GameJava1_08_JFrame frame = new GameJava1_08_JFrame();
 		frame.setVisible(true);		 
 	}
  
-	public GameJava1_08_JFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public GameJava1_08_JFrame() { 
 		setBounds(100, 100, 400, 300);
-		contentPane = new JPanel(); 
+		JPanel panel = new JPanel(); 
 		
-		JLabel label = new JLabel("처음 만들어 본 자바 애플릿");
-		label.setHorizontalAlignment(JLabel.CENTER); 
-		contentPane.add(label);
+		JLabel label = new JLabel("처음 만들어 본 자바 애플릿"); 
+		panel.add(label);
 
-		setContentPane(contentPane); 
-		
+		setContentPane(panel);  
 	}
 
 }
